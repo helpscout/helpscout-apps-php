ef667e36869cff1f32020d7c402f3160dd95cbb5
Tom Graham <mail@tomgraham.engineer>
2016-09-22 18:52:20 +0100

    Fix User role property assignment and boolean getters.
    The role was being cast to an int but the webhook now sends the user role as the string value (user, admin or owner).

---
+++ tests/HelpScoutApp/model/UserTest.php
@@ -0,0 +1,89 @@
+<?php
+namespace HelpScoutApp\model;
+
+class UserTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRoleAllowsStrings()
+    {
+        $user = new User((object) array(
+            'role' => 'user',
+        ));
+
+        $this->assertSame('user', $user->getRole());
+    }
+
+    /**
+     * @dataProvider isOwnerOrAdminProvider
+     */
+    public function testIsOwnerOrAdmin($role, $expectedResult)
+    {
+        $user = new User((object) array(
+            'role' => $role,
+        ));
+
+        $this->assertSame($expectedResult, $user->isOwnerOrAdmin());
+    }
+
+    public function isOwnerOrAdminProvider()
+    {
+        return array(
+            array('user', false),
+            array('admin', true),
+            array('owner', true),
+        );
+    }
+
+    /**
+     * @dataProvider isOwnerProvider
+     */
+    public function testIsOwner($role, $expectedResult)
+    {
+        $user = new User((object) array(
+            'role' => $role,
+        ));
+
+        $this->assertSame($expectedResult, $user->isOwner());
+    }
+
+    public function isOwnerProvider()
+    {
+        return array(
+            array('user', false),
+            array('admin', false),
+            array('owner', true),
+        );
+    }
+
+    /**
+     * @dataProvider isAdminProvider
+     */
+    public function testIsAdmin($role, $expectedResult)
+    {
+        $user = new User((object) array(
+            'role' => $role,
+        ));
+
+        $this->assertSame($expectedResult, $user->isAdmin());
+    }
+
+    public function isAdminProvider()
+    {
+        return array(
+            array('user', false),
+            array('admin', true),
+            array('owner', false),
+        );
+    }
+
+
+    public function isOwnerOrAdmin() {
+        return in_array($this->role, array(self::ROLE_OWNER, self::ROLE_ADMIN));
+    }
+
+    public function isOwner() {
+        return $this->role == self::ROLE_OWNER;
+    }
+    public function isAdmin() {
+        return $this->role == self::ROLE_ADMIN;
+    }
+}
